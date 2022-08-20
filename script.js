fb78692c1f9abb10e6d261e873ef4bc66c301a48
DarleyRaffael <darleyraffael@gmail.com>
2022-08-20 19:35:11 -0300

    dia2 versao 3.0

---
+++ script.js
@@ -0,0 +1,4 @@
+        const $start = document.getElementById('start')
+        const $foquete = document.getElementById('rocket')
+          $start.addEventListener('change', () => {
+          $foquete.classList.toggle('foguete-fly')})
